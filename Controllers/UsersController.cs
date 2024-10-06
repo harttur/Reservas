432f2ddd6a0f2284871aa6298c37a5ef7ed6e32e
Harttur <harttur.diogenes@sga.pucminas.br>
2024-10-06 19:47:28 -0300

    Feat: adiciona configuração para conexão com o banco de dados;

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Reservas.Dtos;
+using Reservas.Services.Contract;
+using ZstdSharp.Unsafe;
+
+namespace Reservas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<UserDto>>> GetAllUsers()
+        {
+            var users = await _userService.GetAllUsersAsync();
+            return users;
+        }
+
+        [HttpGet("{id_user}")]
+        public async Task<ActionResult<UserDto>> GetUserById(string id_user)
+        { 
+            var user = await _userService.GetUserByIdAsync(id_user);
+
+            if(user == null)
+            {
+                return NotFound();    
+            }
+
+            return user; 
+        }
+ 
+        [HttpPost]
+        public async Task<ActionResult> CreateUser(UserDto userDto)
+        { 
+            await _userService.CreateUserAsync(userDto); 
+            return CreatedAtAction(nameof(GetUserById),new { id_user = userDto.Id_user}, userDto);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(string id_user, UserDto userdto)
+        {
+            await _userService.UpdateUserAsync(id_user, userdto);
+            return NoContent(); 
+
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult> DeleteUser(string id_user)
+        { 
+            await _userService.DeleteUserAsync(id_user);
+            return NoContent();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,22 @@
+using MongoDB.Driver;
+using Reservas.Configurations;
 using Reservas.Repository;
 using Reservas.Repository.Contract;
 using Reservas.Services;
 using Reservas.Services.Contract;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Lougin string connection DB
+var mongoConnectionString = builder.Configuration.GetConnectionString("MongoDbConnection");
+var mongoDataBaseName = builder.Configuration["MongoDbSettings:DatabaseName"];
+builder.Services.AddSingleton<IMongoClient, MongoClient>(sp => new MongoClient(mongoConnectionString));
+builder.Services.AddScoped(sp =>
+{
+    var client = sp.GetRequiredService<IMongoClient>();
+    return client.GetDatabase(mongoDataBaseName);
+});
+
 
 // Add services to the container.
 builder.Services.AddScoped<IUserRepository, UserRepository>();
